224429deebb0cf1d496a5ace318cb210241dddaa
Stefan Silviu <sylviu44@gmail.com>
2018-12-17 11:01:49 +0200

    Update mousealign.sh to auto-detect mouse id

--- files/Scripts/mousealign.sh
+++ files/Scripts/mousealign.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+mouseId=$(xinput | grep -E "^⎜.*?USB Gaming Mouse\s\s" | head -n1 | cut -f 2 | cut -d= -f2)
 # Disable mouse acceleration
-xinput set-prop 10 "libinput Accel Profile Enabled" 0, 1
-xinput set-prop 10 "libinput Accel Speed" 0
+xinput set-prop $mouseId "libinput Accel Profile Enabled" 0, 1
+xinput set-prop $mouseId "libinput Accel Speed" 0
+# Default speed, let mouse deal with it
+xinput set-prop $mouseId "libinput Accel Speed" 0
+
