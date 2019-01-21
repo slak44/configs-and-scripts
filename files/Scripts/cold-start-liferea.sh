2f1accd6d7c3fe4a2b493dae5fa3b8b36f686d64
Stefan Silviu <sylviu44@gmail.com>
2019-01-21 16:03:57 +0200

    Fix cold-start-liferea.sh

--- files/Scripts/cold-start-liferea.sh
+++ files/Scripts/cold-start-liferea.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-DISPLAY=:0 XAUTHORITY="$HOME/.Xauthority" XDG_RUNTIME_DIR=/run/user/1000 liferea &
-sleep 1
+XDG_RUNTIME_DIR=/run/user/1000 XAUTHORITY="/home/slak/.Xauthority" DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus DISPLAY=:0 liferea &
+sleep 2
 DISPLAY=:0 wmctrl -r "Liferea" -b toggle,shaded
