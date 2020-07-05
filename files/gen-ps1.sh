5fa7f10125ff1d7b844c81e674b6261aa83a9fad
Stefan Silviu <sylviu44@gmail.com>
2020-07-05 12:06:21 +0300

    Improve PS1 generator for long text

--- files/gen-ps1.sh
+++ files/gen-ps1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-isGoodTerminal=$([ $TERM_NAME == 'konsole' ] || [ $TERM_NAME == 'java' ] && echo true)
-noFiraCode=$([ ! $isGoodTerminal ] && echo true)
-hasTrueColor=$([ "$(echo $1 | head -c 1)" = "-" ] && echo "false" || echo "true")
+isGoodTerminal=$([[ ${TERM_NAME} == 'konsole' ]] || [[ ${TERM_NAME} == 'java' ]] && echo true)
+noFiraCode=$([[ ! ${isGoodTerminal} ]] && echo true)
+hasTrueColor=$([[ "$(echo $1 | head -c 1)" = "-" ]] && echo "false" || echo "true")
 
-isRoot=$([ $EUID -eq 0 ] && echo true)
+isRoot=$([[ $EUID -eq 0 ]] && echo true)
 
-base=$([ "$hasTrueColor" = "true" ] && echo "\[\033[38;2;0;231;255m\]" || echo "\[\033[38;2;0;0;175m\]")
-accent=$([ "$hasTrueColor" = "true" ] && echo "\[\033[38;2;255;132;42m\]" || echo "\[\033[38;2;255;119;0m\]")
+base=$([[ "$hasTrueColor" = "true" ]] && echo "\[\033[38;2;0;231;255m\]" || echo "\[\033[38;2;0;0;175m\]")
+accent=$([[ "$hasTrueColor" = "true" ]] && echo "\[\033[38;2;255;132;42m\]" || echo "\[\033[38;2;255;119;0m\]")
 white="\[\033[38;2;255;255;255m\]"
 reset="\[\033[0m\]"
-user=$([ $isRoot ] && echo "$white" || echo "$base")
+user=$([[ ${isRoot} ]] && echo "$white" || echo "$base")
 
-sep=$([ $noFiraCode ] && echo "$accent>" || echo -e "$accent\356\202\261")
-branchIcon=$([ $noFiraCode ] && echo "${white}branch " || echo -e "${white}\356\202\240")
+sep=$([[ ${noFiraCode} ]] && echo "$accent>" || echo -e "$accent\356\202\261")
+branchIcon=$([[ ${noFiraCode} ]] && echo "${white}branch " || echo -e "${white}\356\202\240")
 
 declare -a upstream
 declare -a branchAB
@@ -26,23 +26,46 @@ while read line; do
 done <<< "$(git status --branch --porcelain=v2 2> /dev/null)"
 
 simpleBranchText="${branchIcon}${reset}${branch} $sep "
-localBranchText=$([ ${upstream[0]} ] && echo "${reset}local $simpleBranchText" || echo "$simpleBranchText")
-upstreamBranchText=$([ ${upstream[0]} ] && echo "${reset}remote ${branchIcon}${reset}${upstream[1]} from ${base}${upstream[0]}${reset} $sep " || echo '')
-branchText=$([ $branch ] && echo "$localBranchText$upstreamBranchText" || echo '')
+localBranchText=$([[ ${upstream[0]} ]] && echo "${reset}local $simpleBranchText" || echo "$simpleBranchText")
+upstreamBranchText=$([[ ${upstream[0]} ]] && echo "${reset}remote ${branchIcon}${reset}${upstream[1]} from ${base}${upstream[0]}${reset} $sep " || echo '')
+branchText=$([[ ${branch} ]] && echo "$localBranchText$upstreamBranchText" || echo '')
 if [[ ${branchAB[0]} -ne 0 ]]; then
-  pluralCommits=$([ ${branchAB[0]} -eq 1 ] && echo "commit" || echo "commits")
+  pluralCommits=$([[ ${branchAB[0]} -eq 1 ]] && echo "commit" || echo "commits")
   ahead="${base}${branchAB[0]}${reset} ${pluralCommits} ${white}ahead ${sep} "
 fi
 if [[ ${branchAB[1]} -ne 0 ]]; then
-  pluralCommits=$([ ${branchAB[1]} -eq 1 ] && echo "commit" || echo "commits")
+  pluralCommits=$([[ ${branchAB[1]} -eq 1 ]] && echo "commit" || echo "commits")
   behind="${base}${branchAB[1]}${reset} ${pluralCommits} ${white}behind ${sep} "
 fi
-workingTreeDirty=$([ $changed ] && echo "${reset}working tree ${base}dirty ${sep} " || echo '')
+workingTreeDirty=$([[ ${changed} ]] && echo "${reset}working tree ${base}dirty ${sep} " || echo '')
 
-echo "$sep $user\u$accent@$base\h $sep $base\w $sep ${branchText}${reset}${ahead}${behind}${workingTreeDirty}${reset}"
+commitsAndWorktreeText="${ahead}${behind}${workingTreeDirty}"
 
-#FIXME
-#approxTextLength=$(awk -v toSplit="${#branch} ${#upstream[0]} ${#upstream[1]} ${#branchAB[0]} ${#branchAB[1]}" \
-#  'BEGIN { s = 0; t = split(toSplit, a, " "); for (i = 1; i <= t; i++) s += a[i]; print s; }')
+function removeExtraStuffLength() {
+  hexLetters=$(echo -ne "$1" | sed 's/\x1B\[[0-9;]\+[A-Za-z]//g' | xxd -p -c 999 | sed "s/5c5b5c5d//g" | wc -m)
+  echo $((hexLetters / 4))
+}
 
-[ ${upstream[0]} ] && echo "$sep ${reset}command $sep ${reset}"
+pwdLen=$(pwd | wc -m)
+preludeMaxLen=$((25 + pwdLen))
+totalLength=$(tput cols)
+freeSpace=$((totalLength - preludeMaxLen))
+branchTextLength=$(removeExtraStuffLength "$branchText")
+commitsAndWorktreeLength=$(removeExtraStuffLength "$commitsAndWorktreeText")
+
+spaceLeftForCmd=$((freeSpace - branchTextLength - commitsAndWorktreeLength))
+shouldPutCmdOnNext=$([[ $((spaceLeftForCmd < totalLength / 2)) == "1" ]] && echo "true" || echo '')
+
+firstLineLen=$((preludeMaxLen + branchTextLength + commitsAndWorktreeLength))
+
+# Print anyway
+echo -n "$sep $user\u$accent@$base\h $sep $base\w $sep ${branchText}"
+
+if [[ $((firstLineLen >= totalLength)) == "1" ]]; then
+  # Put newline, and separator for next line
+  echo
+  echo -n "$sep "
+fi
+echo "${reset}${commitsAndWorktreeText}${reset}"
+
+[[ ${shouldPutCmdOnNext} ]] && echo "$sep ${reset}command $sep ${reset}"
