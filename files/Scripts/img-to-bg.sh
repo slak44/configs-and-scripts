5582af70891a4e5414d8208233f6203268738900
Stefan Silviu <stefan.silviu.alexandru@gmail.com>
2024-06-02 19:11:35 +0300

    Improve background generation script

--- files/Scripts/img-to-bg.sh
+++ files/Scripts/img-to-bg.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 for path in "$@"; do
   mkdir -p "$(dirname "$path")"/edited
-  target_path=`dirname "$path"`/edited/`basename "$path"`
+  target_path=$(dirname "$path")/edited/$(basename "$path")
   echo "$path -> $target_path"
-  convert "$path" -resize 1920x1080 -gravity Center -background black -extent 5760x1080 -format png "$target_path"
+
+  main_resolution=$(xrandr | grep -oP 'primary \K\d+[x]\d+')
+  full_resolution=$(xdpyinfo | grep -oP 'dimensions:\s+\K\S+')
+
+  # 2560x1440 and 6400x1440
+  convert "$path" -resize "$main_resolution" -gravity Center -background black -extent "$full_resolution" -format png "$target_path"
 done
